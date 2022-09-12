9fd7123879b84e1c10028d447d0d01c1db28d749
berke <bab26@st-andrews.ac.uk>
2022-09-12 16:49:34 +0300

    feat: Add auth for edit and delete jokes

--- Joke-Notes-MVC/Controllers/JokesController.cs
+++ Joke-Notes-MVC/Controllers/JokesController.cs
@@ -72,6 +72,7 @@ namespace Joke_Notes_MVC.Controllers
         // POST: Jokes/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,JokeQuestion,JokeAnswer")] Joke joke)
@@ -86,6 +87,7 @@ namespace Joke_Notes_MVC.Controllers
         }
 
         // GET: Jokes/Edit/5
+        [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null || _context.Joke == null)
@@ -104,6 +106,7 @@ namespace Joke_Notes_MVC.Controllers
         // POST: Jokes/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,JokeQuestion,JokeAnswer")] Joke joke)
@@ -137,6 +140,7 @@ namespace Joke_Notes_MVC.Controllers
         }
 
         // GET: Jokes/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null || _context.Joke == null)
@@ -155,6 +159,7 @@ namespace Joke_Notes_MVC.Controllers
         }
 
         // POST: Jokes/Delete/5
+        [Authorize]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
