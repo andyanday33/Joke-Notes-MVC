8cbe8cc4247dacadef98b5ba0a5b6eee82ce6435
berke <bab26@st-andrews.ac.uk>
2022-09-12 14:15:03 +0300

    Add project files.

---
+++ Joke-Notes-MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Joke_Notes_MVC.Models;
+
+namespace Joke_Notes_MVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Joke_Notes_MVC.Models.Joke>? Joke { get; set; }
+    }
+}
---
+++ Joke-Notes-MVC/Models/Joke.cs
@@ -0,0 +1,18 @@
+﻿namespace Joke_Notes_MVC.Models
+{
+    public class Joke
+    {
+
+        public int Id { get; set; }
+
+        public string JokeQuestion { get; set; }
+
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+
+    }
+}
